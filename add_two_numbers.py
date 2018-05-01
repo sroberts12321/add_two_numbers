92c31306713fec395d396798fc2cbf0455b7ad0b
Stephen Roberts <Sroberts12321@gmail.com>
2018-05-01 11:47:23 -0500

    the completed program

---
+++ add_two_numbers.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter another number: "))
+print("\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\ndoing VERY complicated math behind the scenes")
+print(".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.")
+result = num1 + num2
+print("Phew! That was rough, but I added your numbers. Here is the result: " + str(result))
